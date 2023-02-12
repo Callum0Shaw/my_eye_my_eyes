f536717f334b88e238255d72b620e09747781373
Callum Shaw <callum0shaw@gmail.com>
2023-02-12 19:36:43 +0200

    init: Test basic storyblok implmentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
+import vercel from "@astrojs/vercel/serverless";
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        card: 'storyblok/Card'
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: 'us', // optional,  or 'eu' (default)
-      },
-    })
-  ],
+  integrations: [storyblok({
+    // accessToken: env.STORYBLOK_TOKEN,
+    accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
+    components: {
+      page: "storyblok/Page",
+    feature: "storyblok/Feature",
+    grid: "storyblok/Grid",
+    teaser: "storyblok/Teaser",
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'us' // optional,  or 'eu' (default)
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
