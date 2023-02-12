e93e2466b60521fec207036de2cc7338ee677885
Callum Shaw <callum0shaw@gmail.com>
2023-02-12 19:24:17 +0200

    init: Temporary storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        card: 'storyblok/Card'
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'us', // optional,  or 'eu' (default)
+      },
+    })
+  ],
+});
