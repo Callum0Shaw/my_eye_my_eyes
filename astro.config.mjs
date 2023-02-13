bf73296771868aae8f779cb6f113e3d312363926
Callum Shaw <callum0shaw@gmail.com>
2023-02-13 16:55:13 +0200

    feat: delete storyblok template code to start from scratch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,4 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import vercel from "@astrojs/vercel/serverless";
-
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [storyblok({
-    // accessToken: env.STORYBLOK_TOKEN,
-    accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
-    bridge: true,
-    components: {
-      page: "storyblok/Page",
-      feature: "storyblok/Feature",
-      grid: "storyblok/Grid",
-      teaser: "storyblok/Teaser",
-    }
-  })],
-  output: "server",
-  adapter: vercel()
-});
+export default defineConfig({});
